1afe90206ede87577e2b86b626604b7ca6c14fdf
ybressler <yaakovbressler@gmail.com>
2022-01-17 18:35:19 -0500

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,17 @@
 module.exports = {
     siteMetadata: {
+        title: "My First Gatsby Site",
         siteUrl: `https://www.test.yaakovbressler.com`,
     },
     plugins: [
       "gatsby-plugin-image",
-      "gatsby-plugin-sharp"
+      "gatsby-plugin-sharp",
+      {
+        resolve: "gatsby-source-filesystem",
+        options: {
+          name: `blog`,
+          path: `${__dirname}/blog`,
+        }
+      }
     ]
 }
