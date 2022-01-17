bc91c36f8c1f8f5b524bab63ecb97122da24a454
ybressler <yaakovbressler@gmail.com>
2022-01-17 15:26:52 -0500

    Finished Gatsby Tutorial Part 2

--- upload_to_gcp.sh
+++ upload_to_gcp.sh
@@ -1,2 +1,7 @@
+# Define parallel processing variables
+export GATSBY_CPU_COUNT='physical_cores';
+
+# First build the site
+gatsby build;
 # This script uploads all contents from "public" to GCP
 (cd public; gsutil -m cp -r * gs://test.yaakovbressler.com)
