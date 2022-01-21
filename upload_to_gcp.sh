bf0da3fc04adff392b301ba2b91e92b1543ba8f7
ybressler <yaakovbressler@gmail.com>
2022-01-20 19:02:38 -0500

    change script to remove all contents from bucket, then replace. (Not a good approach, but who cares for now)

--- upload_to_gcp.sh
+++ upload_to_gcp.sh
@@ -3,5 +3,9 @@ export GATSBY_CPU_COUNT='physical_cores';
 
 # First build the site
 gatsby build;
+
+# Remove all objects and their versions from the bucket – without deleting the bucket
+(cd public; gsutil -m rm -a gs://test.yaakovbressler.com/**)
+
 # This script uploads all contents from "public" to GCP
 (cd public; gsutil -m cp -r * gs://test.yaakovbressler.com)
